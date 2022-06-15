c3ac9e9aa66327540c0f4fabf315977b48c23c1b
Shaheryar Qaseer <shaheryar.qaseer@codeninja.pk>
2022-06-15 18:34:00 +0500

    issue fixed

--- src/components/quill-editor.js
+++ src/components/quill-editor.js
@@ -1,98 +1,97 @@
-import 'react-quill/dist/quill.snow.css';
-import dynamic from 'next/dynamic';
-import { styled } from '@mui/material/styles';
-import PropTypes from 'prop-types';
-import { useRef } from 'react';
+import "react-quill/dist/quill.snow.css";
+import dynamic from "next/dynamic";
+import { styled } from "@mui/material/styles";
+import PropTypes from "prop-types";
+import { useRef } from "react";
+import { Quill } from "react-quill";
 
-const Quill = dynamic(() => import('react-quill'), { ssr: false });
-
-const QuillEditorRoot = styled('div')(({ theme }) => ({
+const QuillEditorRoot = styled("div")(({ theme }) => ({
   border: 1,
   borderColor: theme.palette.divider,
   borderRadius: theme.shape.borderRadius,
-  borderStyle: 'solid',
-  display: 'flex',
-  flexDirection: 'column',
-  overflow: 'hidden',
-  '& .quill': {
-    display: 'flex',
+  borderStyle: "solid",
+  display: "flex",
+  flexDirection: "column",
+  overflow: "hidden",
+  "& .quill": {
+    display: "flex",
     flex: 1,
-    flexDirection: 'column',
-    overflow: 'hidden'
+    flexDirection: "column",
+    overflow: "hidden",
   },
-  '& .ql-snow.ql-toolbar': {
+  "& .ql-snow.ql-toolbar": {
     borderColor: theme.palette.divider,
-    borderLeft: 'none',
-    borderRight: 'none',
-    borderTop: 'none',
-    '& .ql-picker-label:hover': {
-      color: theme.palette.primary.main
-    },
-    '& .ql-picker-label.ql-active': {
-      color: theme.palette.primary.main
-    },
-    '& .ql-picker-item:hover': {
-      color: theme.palette.primary.main
-    },
-    '& .ql-picker-item.ql-selected': {
-      color: theme.palette.primary.main
-    },
-    '& button:hover': {
+    borderLeft: "none",
+    borderRight: "none",
+    borderTop: "none",
+    "& .ql-picker-label:hover": {
       color: theme.palette.primary.main,
-      '& .ql-stroke': {
-        stroke: theme.palette.primary.main
-      }
     },
-    '& button:focus': {
+    "& .ql-picker-label.ql-active": {
       color: theme.palette.primary.main,
-      '& .ql-stroke': {
-        stroke: theme.palette.primary.main
-      }
     },
-    '& button.ql-active': {
-      '& .ql-stroke': {
-        stroke: theme.palette.primary.main
-      }
+    "& .ql-picker-item:hover": {
+      color: theme.palette.primary.main,
     },
-    '& .ql-stroke': {
-      stroke: theme.palette.text.primary
+    "& .ql-picker-item.ql-selected": {
+      color: theme.palette.primary.main,
     },
-    '& .ql-picker': {
-      color: theme.palette.text.primary
+    "& button:hover": {
+      color: theme.palette.primary.main,
+      "& .ql-stroke": {
+        stroke: theme.palette.primary.main,
+      },
     },
-    '& .ql-picker-options': {
+    "& button:focus": {
+      color: theme.palette.primary.main,
+      "& .ql-stroke": {
+        stroke: theme.palette.primary.main,
+      },
+    },
+    "& button.ql-active": {
+      "& .ql-stroke": {
+        stroke: theme.palette.primary.main,
+      },
+    },
+    "& .ql-stroke": {
+      stroke: theme.palette.text.primary,
+    },
+    "& .ql-picker": {
+      color: theme.palette.text.primary,
+    },
+    "& .ql-picker-options": {
       backgroundColor: theme.palette.background.paper,
-      border: 'none',
+      border: "none",
       borderRadius: theme.shape.borderRadius,
       boxShadow: theme.shadows[10],
-      padding: theme.spacing(2)
-    }
+      padding: theme.spacing(2),
+    },
   },
-  '& .ql-snow.ql-container': {
-    borderBottom: 'none',
+  "& .ql-snow.ql-container": {
+    borderBottom: "none",
     borderColor: theme.palette.divider,
-    borderLeft: 'none',
-    borderRight: 'none',
-    display: 'flex',
+    borderLeft: "none",
+    borderRight: "none",
+    display: "flex",
     flex: 1,
-    flexDirection: 'column',
-    height: 'auto',
-    overflow: 'hidden',
-    '& .ql-editor': {
+    flexDirection: "column",
+    height: "auto",
+    overflow: "hidden",
+    "& .ql-editor": {
       color: theme.palette.text.primary,
       flex: 1,
       fontFamily: theme.typography.body1.fontFamily,
       fontSize: theme.typography.body1.fontSize,
-      height: 'auto',
-      overflowY: 'auto',
+      height: "auto",
+      overflowY: "auto",
       padding: theme.spacing(2),
-      '&.ql-blank::before': {
+      "&.ql-blank::before": {
         color: theme.palette.text.secondary,
-        fontStyle: 'normal',
-        left: theme.spacing(2)
-      }
-    }
-  }
+        fontStyle: "normal",
+        left: theme.spacing(2),
+      },
+    },
+  },
 }));
 
 export const QuillEditor = (props) => {
@@ -100,10 +99,7 @@ export const QuillEditor = (props) => {
   const ref = useRef(null);
 
   return (
-    <QuillEditorRoot
-      sx={sx}
-      ref={ref}
-      {...other}>
+    <QuillEditorRoot sx={sx} ref={ref} {...other}>
       <Quill
         onChange={onChange}
         placeholder={placeholder}
@@ -118,5 +114,5 @@ QuillEditor.propTypes = {
   onChange: PropTypes.func,
   placeholder: PropTypes.string,
   sx: PropTypes.object,
-  value: PropTypes.string
+  value: PropTypes.string,
 };
